c28a8bfb0569d21cf7756dde6cf2fe9cdd1790b4
arnoud <arnoud@airsupplies.nl>
2022-08-25 16:32:53 +0200

    forget commit

---
+++ components/wifi/WifiCtrMode.h
@@ -0,0 +1,17 @@
+/*
+ * WifiMode.h
+ *
+ *  Created on: Aug 23, 2022
+ *      Author: arnoud
+ */
+
+#ifndef COMPONENTS_WIFI_WIFICTRMODE_H_
+#define COMPONENTS_WIFI_WIFICTRMODE_H_
+
+enum class WifiCtrMode
+	{
+	WifiAP = 1,
+	WifiSTA = 2
+	};
+
+#endif /* COMPONENTS_WIFI_WIFICTRMODE_H_ */
